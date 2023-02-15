a196c98b7ffc2cb3eebe35d5eddc7d168edec48a
dekken <philip.deegan@gmail.com>
2023-02-15 23:58:37 +0100

    v0.0.3

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import find_packages, setup
 
 setup(
     name="phlop",
-    version="0.0.2",
+    version="0.0.3",
     cmdclass={},
     classifiers=[],
     include_package_data=True,
