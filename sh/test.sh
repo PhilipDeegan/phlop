4fde2fe04374715ee48256765dee47cce7a4740b
dekken <philip.deegan@gmail.com>
2024-02-12 14:10:13 +0100

    updates

--- sh/test.sh
+++ sh/test.sh
@@ -5,5 +5,6 @@ cd "$CWD"/..
 
 set -ex
 
-PYTHONPATH=$PWD python3 -m phlop.run.test_cases -d tests -p
-PYTHONPATH=$PWD python3 -m phlop.run.test_cases -d tests -c 10
+PYTHONPATH=$PWD python3 -Om phlop.run.test_cases -d tests -p
+PYTHONPATH=$PWD python3 -Om phlop.run.test_cases -d tests -c 10
+PYTHONPATH=$PWD python3 -O tests/all_concurrent.py
