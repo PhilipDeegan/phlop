7f08409451db96a70c5971735793be941995e068
PhilipDeegan <philip.deegan@gmail.com>
2024-03-02 16:09:32 +0000

    bump (#12)

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import find_packages, setup
 
 setup(
     name="phlop",
-    version="0.0.11",
+    version="0.0.12",
     cmdclass={},
     classifiers=[],
     include_package_data=True,
