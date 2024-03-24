8077c2fac23471df425c844b79ae916f870a7f3e
dekken <philip.deegan@gmail.com>
2024-03-24 18:10:46 +0100

    allow import error with env override

--- phlop/reflection.py
+++ phlop/reflection.py
@@ -12,9 +12,14 @@ from pathlib import Path
 
 from phlop.sys import extend_sys_path
 
+FORCE_RAISE_ON_IMPORT_ERROR = os.getenv(
+    "PHLOP_FORCE_RAISE_ON_IMPORT_ERROR", "False"
+).lower() in ("true", "1", "t")
 
-def classes_in_file(file, subclasses_only=None, fail_on_import_error=True):
-    assert file
+
+def classes_in_file(file_path, subclasses_only=None, fail_on_import_error=False):
+    file = Path(file_path)
+    assert file.exists()
     module = str(file).replace(os.path.sep, ".")[:-3]
     assert module
 
@@ -23,7 +28,7 @@ def classes_in_file(file, subclasses_only=None, fail_on_import_error=True):
 
     classes = []
 
-    with extend_sys_path(os.getcwd()):
+    with extend_sys_path([os.getcwd(), str(file.parent)]):
         try:
             for name, cls in inspect.getmembers(
                 importlib.import_module(module), inspect.isclass
@@ -34,7 +39,7 @@ def classes_in_file(file, subclasses_only=None, fail_on_import_error=True):
                 if should_add:
                     classes += [cls]
         except (ValueError, ModuleNotFoundError) as e:
-            if fail_on_import_error:
+            if fail_on_import_error or FORCE_RAISE_ON_IMPORT_ERROR:
                 raise e
             logging.error(f"Skipping on error: {e} in module {module}")
 
