899037d79971bb213715f49d3fcc28a75760e1a5
PhilipDeegan <philip.deegan@gmail.com>
2024-03-25 14:25:24 +0000

    fix wheel (#17)

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name="phlop",
+    version="0.0.16",
+    cmdclass={},
+    classifiers=[],
+    include_package_data=True,
+    packages=find_packages(exclude=["lib/", "tests/"]),
+)
