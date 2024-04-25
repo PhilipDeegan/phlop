904032b30ba1c75731659382bdac4a7cd51613cd
dekken <philip.deegan@gmail.com>
2024-04-25 17:05:59 +0200

    python init files

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import find_packages, setup
 
 setup(
     name="phlop",
-    version="0.0.18",
+    version="0.0.19",
     cmdclass={},
     classifiers=[],
     include_package_data=True,
