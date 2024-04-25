bf4d3f3f4365051212da5c2025217878ba4e0d3d
dekken <philip.deegan@gmail.com>
2024-04-25 16:08:47 +0200

    bump v0.0.18

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import find_packages, setup
 
 setup(
     name="phlop",
-    version="0.0.17",
+    version="0.0.18",
     cmdclass={},
     classifiers=[],
     include_package_data=True,
