a45955f01d130166383c00d26fdeca15fef3c130
PhilipDeegan <philip.deegan@gmail.com>
2024-09-28 15:49:07 +0200

    variance across scope timing files (naive)

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import find_packages, setup
 
 setup(
     name="phlop",
-    version="0.0.26",
+    version="0.0.27",
     cmdclass={},
     classifiers=[],
     include_package_data=True,
